34611368b9ad303e618982233b1c12450e032bd5
Jobin Lawrance <jobinlawrance@gmail.com>
2017-12-18 09:40:47 +0530

    use metric units in weather api

--- app/src/main/java/com/jobinlawrance/weather/data/network/WeatherApi.kt
+++ app/src/main/java/com/jobinlawrance/weather/data/network/WeatherApi.kt
@@ -9,6 +9,6 @@ import retrofit2.http.Query
  * Created by jobinlawrance on 16/12/17.
  */
 interface WeatherApi {
-    @GET("data/2.5/weather?appid=6b43a289b325a4dc60798afb111cff0e")
+    @GET("data/2.5/weather?appid=6b43a289b325a4dc60798afb111cff0e&units=metric")
     fun getWeatherForLatLng(@Query("lat") lat: Double,@Query("lon") lng: Double): Observable<WeatherResponse>
 }
